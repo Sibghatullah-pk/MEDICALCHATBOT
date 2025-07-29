34cc4729c30fa66f9c672ba3c6836b7062da2a58
Sibghatullah-pk <sibghatullah1a2a3a@gmail.com>
2025-07-29 12:27:03 +0500

    seting up  the enviroment

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src
+mkdir -p research
+
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.py
+
+echo "directory and files cretaed"
+
+#open gitbash from here and run sh and file name
